301b4570224c9de05d67b3dfa2fc6651f1f1fc0c
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-06-01 21:42:53 -0400

    Add empty file for leetcode problem 5
    Title says it all.

---
+++ leetcode/qqy/LongestPalindromicSubstring/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        string longestPalindrome(string s) {
+
+        }
+};
+
+int main()
+{
+    Solution s;
+    return 0;
+}
+
