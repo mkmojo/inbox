5161da4e5e9645d45e720c38a91e70ce68dd0eb7
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-06-04 21:46:43 -0400

    Add solution to problem
    This solution has a o(n^3) complexity.
    o(n^2) for iterating through all possible positions of starting
    and finishing points.
    o(n) for testing if some string is palindrome save a bit with
    memerization but not much.

--- leetcode/qqy/LongestPalindromicSubstring/main.cpp
+++ leetcode/qqy/LongestPalindromicSubstring/main.cpp
@@ -7,15 +7,66 @@
 using namespace std;
 
 class Solution {
-    public:
-        string longestPalindrome(string s) {
 
+    int isPalindrome(string s){
+        int i=0;
+        int j=s.size();
+        while(i<j){
+            if(s[i] != s[j])
+                return 0;
+            i++;
+            j--;
         }
+        return s.size();
+    }
+    public:
+    string longestPalindrome(string s) {
+        if(s.size() <= 1) return s;
+        int dp[s.size()][s.size()];
+        for(size_t i=0;i<s.size();i++)
+            for(size_t j=0;j<s.size();j++)
+                dp[i][j] = -1;
+
+        string res = "";
+        for(size_t i=1;i<s.size();i++){
+            for(size_t j=0;i-j+1>res.size();j++){
+                dp[j][i] = 0;
+                if(s[j] == s[i]){
+                    if(dp[j+1][i-1] == -1){
+
+                        dp[j][i] = (dp[j+1][i-1] = 
+                                isPalindrome(s.substr(j+1, i-j-1))) + 2;
+
+                        if( i-j+1 > res.size())
+                            res = s.substr(j, i-j+1);
+                    }else if(dp[j+1][i-1] == 0){
+                        dp[j][i] = 0;
+                    }else if(dp[j+1][i-1] > 0){
+                        dp[j][i] = dp[j+1][i-1] + 2;
+                        if( i-j+1 > res.size())
+                            res = s.substr(j, i-j+1);
+                    } else{
+                        cout << "erroe" <<endl;
+                    }
+
+
+                }
+            }
+        }
+        return res;
+    }
 };
 
 int main()
 {
     Solution s;
+    cout << s.longestPalindrome("xxabccbatt") <<endl;
+    cout << s.longestPalindrome("bccbatt") <<endl;
+    cout << s.longestPalindrome("aaaaaaa") <<endl;
+    cout << s.longestPalindrome("") <<endl;
+    cout << s.longestPalindrome("xxabcdefgh") <<endl;
+    cout << s.longestPalindrome("abcdefghxx") <<endl;
+    cout << s.longestPalindrome("aaaacdefgbbbbbkkle") <<endl;
     return 0;
 }
 
