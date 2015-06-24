0a3b36057c7c6bcbfb66482b03a5dd1e5a675e7a
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-06-23 23:31:02 -0400

    Add logic to enable post redirect and get
    Need to use session and url_for as tools to achieve this pattern.
    The session is a dictionary.

--- ocr/receipt.py
+++ ocr/receipt.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
@@ -19,12 +19,11 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data=''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
