768c6426f9dfa23fd0a6b0b3d70d915d201d285d
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-06-23 22:36:36 -0400

    add in NameForm class

--- ocr/receipt.py
+++ ocr/receipt.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/')
 def index():
+    name = None
     return render_template('index.html')
 
 @app.route('/user/<name>')
