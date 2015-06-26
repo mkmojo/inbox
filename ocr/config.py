3a04c7d7fe45c86f2bd338c6c62c8a8a5ddd0226
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-06-25 21:48:34 -0400

    Fix key error
    Change the key in config.py to match with the used case.

--- ocr/config.py
+++ ocr/config.py
@@ -10,9 +10,9 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[MyBill]'
-    FLASKY_MAIL_SENDER = 'MyBill Admin <admin@mybill.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MYBILL_MAIL_SUBJECT_PREFIX = '[MyBill]'
+    MYBILL_MAIL_SENDER = 'MyBill Admin <admin@mybill.com>'
+    MYBILL_ADMIN = os.environ.get('MYBILL_ADMIN')
 
     @staticmethod
     def init_app(app):
