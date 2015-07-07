80356471dc7bcb422d0c7d6c97c3186babfdf766
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-07-06 21:01:48 -0400

    Move folder creation functionality to run_ocr
    Free the view function from deciding pic_path and text_path.
    These two paths should be decide in the tesseract module.

--- ocr/app/main/run_ocr.py
+++ ocr/app/main/run_ocr.py
@@ -2,20 +2,43 @@ import os
 import subprocess
 from ..models import User, Pic
 from .. import db
+from manage import app
+from time import gmtime, strftime
+from flask import url_for
+from werkzeug import secure_filename
 
-def run_ocr(user, photo_path, text_path,\
+
+def run_ocr(user, form, \
         ocr_engine='tesseract', language = 'eng'):
     """
-    photo_path: input/user uploaded file location
-    text_path: output/interpreted file location
     !!NOTICE: tesseract appends '.txt' to the end of input filename
     """
+
+    #prefix timestamp to filename
+    filename = strftime("%Y-%m-%d-%H:%M:%S_", gmtime()) + \
+            secure_filename(form.photo.data.filename)
+
+    #relative path to static folder
+    relative_pic_path = os.path.join('uploads/', str(user.id),  filename)
+    pic_path = os.path.join(app.config['STATIC_FOLDER'], relative_pic_path)
+
+    relative_text_path = os.path.join('output/', str(user.id),  filename)
+    text_path = os.path.join(app.config['STATIC_FOLDER'], relative_text_path)
+
+    pic_dir_name = os.path.dirname(pic_path)
+    try:
+        if not os.path.exists(pic_dir_name):
+            os.mkdir(pic_dir_name)
+        form.photo.data.save(pic_path)
+    except:
+        print ("failed to save picture to: " + pic_path)
+
     #create directory if not there
     text_dir = os.path.dirname(text_path)
     if not os.path.exists(text_dir):
         os.mkdir(text_dir)
 
-    command = ' '.join([ocr_engine, '-l ' + language, photo_path, text_path])
+    command = ' '.join([ocr_engine, '-l ' + language, pic_path, text_path])
     try:
         subprocess.call(command.split())
     except :
@@ -23,7 +46,8 @@ def run_ocr(user, photo_path, text_path,\
 
     #save statistics to database
     #need to add in hash later in the future for better security
-    pic = Pic(pic_path = photo_path, text_path = text_path + '.txt', owner=user)
+    pic = Pic(pic_path = relative_pic_path, text_path = relative_text_path + \
+            '.txt', owner=user)
     db.session.add(pic)
     db.session.commit()
 
--- ocr/app/main/views.py
+++ ocr/app/main/views.py
@@ -1,13 +1,10 @@
 from flask import render_template, abort
 from flask.ext.login import current_user, login_required
-from werkzeug import secure_filename
 from . import main
 from forms import UploadForm
-from time import gmtime, strftime
 from run_ocr import run_ocr
 from ..models import User, Pic
 from .. import db
-import os
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -21,20 +18,7 @@ def index():
             user = User(id=0)
         else:
             user = User.query.filter_by(id=current_user.get_id()).first_or_404()
-
-        filename = strftime("%Y-%m-%d-%H:%M:%S_", gmtime()) + \
-                secure_filename(form.photo.data.filename)
-        photo_path = os.path.join(os.getcwd(), 'uploads/', str(user.id),  filename)
-        text_path = os.path.join(os.getcwd(), 'output/', str(user.id), filename)
-        photo_dir_name = os.path.dirname(photo_path)
-        try:
-            if not os.path.exists(photo_dir_name):
-                os.mkdir(photo_dir_name)
-            form.photo.data.save(photo_path)
-        except:
-            print ("failed to save picture to: " + photo_path)
-
-        text = run_ocr(user, photo_path, text_path)
+        text = run_ocr(user, form)
     return render_template('index.html', form=form, text=text)
 
 
@@ -44,5 +28,6 @@ def user(user_id):
     user = User.query.filter_by(id=int(user_id)).first()
     if user is None:
         abort(404)
-    pics = Pic.query.order_by(Pic.timestamp.desc()).all()
+    #pics = Pic.query.order_by(Pic.timestamp.desc()).all()
+    pics = user.pics.order_by(Pic.timestamp.desc())
     return render_template('user/user.html', user=user, posts=pics)
