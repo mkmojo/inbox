eed4c1ded3eeb5931b738d20aebf30f0051034a5
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-07-13 15:54:16 -0400

    Adjust link to auth module
    Add the url_path to style sheet and templates folder

--- ocr/app/auth/views.py
+++ ocr/app/auth/views.py
@@ -34,7 +34,7 @@ def login():
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.user', user_id=user.id))
         flash('Invalid username or password.')
-    return render_template('auth/login.html', form=form)
+    return render_template('login.html', form=form)
 
 
 @auth.route('/logout')
