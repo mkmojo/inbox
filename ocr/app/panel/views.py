f09f5268bea51c5c3f9b5f3269749cd8add8540f
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-07-15 11:21:58 -0400

    Fix file upload issue
    The most important part is to have the action=""

--- ocr/app/panel/views.py
+++ ocr/app/panel/views.py
@@ -2,6 +2,8 @@ from flask import render_template, abort
 from flask.ext.login import current_user, login_required
 from ..models import User, Pic
 from . import panel
+from run_ocr import run_ocr
+from forms import UploadForm
 
 @panel.route('/<user_id>')
 @login_required
@@ -12,10 +14,20 @@ def user(user_id):
     pics = user.pics.order_by(Pic.timestamp.desc())
     return render_template("panel/index.html", user=user, posts=pics)
 
-@panel.route('/receipts')
+@panel.route('/receipts', methods=['GET', 'POST'])
 @login_required
 def receipts():
-    return render_template("panel/receipts.html")
+    form = UploadForm()
+    text = ''
+    if form.validate_on_submit():
+        if current_user.is_anonymous():
+            #this is hacky, can work only because the id
+            #for sqlite starts from 1
+            user = User(id=0)
+        else:
+            user = User.query.filter_by(id=current_user.get_id()).first_or_404()
+        text = run_ocr(user, form)
+    return render_template("panel/receipts.html", form=form, text=text )
 
 @panel.route('/transactionss')
 @login_required
