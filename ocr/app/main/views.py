674266f2e89b467ce5ea61c05cb8902cf01ad2a6
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-07-15 11:35:09 -0400

    Move upload to under panel

--- ocr/app/main/views.py
+++ ocr/app/main/views.py
@@ -2,24 +2,13 @@ from flask import render_template, abort
 from flask.ext.login import current_user, login_required
 from . import main
 from forms import UploadForm
-from run_ocr import run_ocr
 from ..models import User, Pic
 from .. import db
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = UploadForm()
-    text = ''
-    if form.validate_on_submit():
-        if current_user.is_anonymous():
-            #this is hacky, can work only because the id
-            #for sqlite starts from 1
-            user = User(id=0)
-        else:
-            user = User.query.filter_by(id=current_user.get_id()).first_or_404()
-        text = run_ocr(user, form)
-    return render_template('main/index.html', form=form, text=text)
+    return render_template('main/index.html')
 
 
 #@main.route('/', defaults={'path':''})
